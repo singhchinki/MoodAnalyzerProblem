911beb6d46f36c197847d7318cdf0139fcc4ec3f
chinki kumari singh <singhchinkikumari.com>
2022-06-10 10:52:33 +0530

    [Chinki] Use Reflection and give parametrized constructor

--- MoodAnalyzer/MoodAnalyzer/MoodAnalyserFactory.cs
+++ MoodAnalyzer/MoodAnalyzer/MoodAnalyserFactory.cs
@@ -10,9 +10,41 @@ namespace MoodAnalyzer
 {
     public class MoodAnalyserFactory
     {
+
+        /// <summary>
+        /// UC- 5- Creates the mood analyse using parameterized constructor.
+        /// </summary>
+        /// Constructor is Not Found
+        /// Class Not Found
+        /// </exception>
+        public static object CreateMoodAnalyseUsingParameterizedConstructor(string className, string constructorName)
+        {
+            Type type = typeof(MoodAnalyser);
+            if (type.Name.Equals(className) || type.FullName.Equals(className))
+            {
+                if (type.Name.Equals(constructorName))
+                {
+                    ConstructorInfo constructor = type.GetConstructor(new[] { typeof(string) });
+                    object instance = constructor.Invoke(new object[] { "Happy" });
+                    return instance;
+                }
+                else
+                {
+                    throw new MoodAnalyzerCustomException(MoodAnalyzerCustomException.ExceptionType.NO_SUCH_METHOD, "Constructor is Not Found");
+
+                }
+            }
+            else
+            {
+                throw new MoodAnalyzerCustomException(MoodAnalyzerCustomException.ExceptionType.NO_SUCH_CLASS, "Class Not Found");
+
+            }
+            
+        }
         /// <summary>
         // TC.4.1 Create MoodAnalyser Method to create object of MoodAnlyser class
-        
+        //UC-4
+
         public static object CreateMoodAnalyser(string ClassName, string constructorName)
         {
             string pattern = @"." + constructorName + "s";
